cd4cd058340ee3756328d016cae6cb705c01941b
Stefan Boos <stefan.boos@gmx.de>
2017-06-18 21:27:44 +0200

    Initial version, based on HTML Boilerplate 5.3.0 and Bootstrap

---
+++ README.md
@@ -0,0 +1,10 @@
+# Fluglärm Überwachung
+
+Diese Anwendung zeichnet die Lärmbelastung durch Flugzeuge auf.
+
+## Bestandteile der Anwendung
+
+### Fluglärm Recorder
+
+Der Recorder zeichnet neue Messwerte auf und stellt sie in einer Web
+Anwendung dar.
---
+++ src/main/java/io/github/wonderbird/aircraftnoise/recorder/App.java
@@ -0,0 +1,36 @@
+package io.github.wonderbird.aircraftnoise.recorder;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
+
+/**
+ * Aircraft Noise Recorder - Application Startup Class.
+ * <p>
+ * Sets up the verticle of the Aircraft Noise Recorder.
+ */
+public class App extends AbstractVerticle {
+
+    @Override
+    public void start(Future<Void> fut) {
+        Router router = Router.router(vertx);
+
+        router.route().handler(StaticHandler.create());
+
+        vertx
+                .createHttpServer()
+                .requestHandler(router::accept)
+                .listen(
+                        config().getInteger("http.port", 8080),
+                        result -> {
+                            if (result.succeeded()) {
+                                fut.complete();
+                            } else {
+                                fut.fail(result.cause());
+                            }
+                        }
+                );
+    }
+
+}

d93668f636532279985c6afd929af8cb9b15ff19
Stefan Boos <kontakt@boos.systems>
2025-08-05 11:58:23 +0200

    feat: read single measurement from DFLD HTML

---
+++ AircraftNoise.Core/Adapters/Outbound/InMemoryMeasurementProvider.cs
@@ -0,0 +1,69 @@
+using AircraftNoise.Core.Domain;
+using HtmlAgilityPack;
+
+namespace AircraftNoise.Core.Adapters.Outbound;
+
+public class InMemoryMeasurementProvider : ICanProvideMeasurements
+{
+    private readonly string _dfldHtmlResponse;
+    private readonly TimeZoneInfo _timeZoneCet = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
+
+    private readonly record struct AreaPayload(int Index, string Title, string Href)
+    {
+        public static AreaPayload Parse(HtmlNode x, int index)
+        {
+            var title = x.GetAttributeValue("title", string.Empty);
+            var href = x.GetAttributeValue("href", string.Empty);
+            
+            // TODO: Throw an exception if title or href is empty (create a test first)
+            
+            return new AreaPayload(index, title, href);
+        }
+    }
+    
+    public InMemoryMeasurementProvider(string dfldHtmlResponse)
+    {
+        _dfldHtmlResponse = dfldHtmlResponse;
+    }
+
+    public Task<IEnumerable<NoiseMeasurement>> GetNoiseMeasurementsForPastTimePeriodAsync(DateTime end, TimeSpan duration)
+    {
+        var html = new HtmlDocument();
+        html.LoadHtml(_dfldHtmlResponse);
+        
+        var areaNodes = html.DocumentNode.SelectNodes("//area");
+
+        var areas = areaNodes.Select(AreaPayload.Parse)
+            .GroupBy(x => x.Index / 2, x => x, (_, areas) =>
+            {
+                // TODO: Hemingway Bridge - Move this to another parser; what about moving the regular expressions from below into that parser?
+                var areaList = areas.ToList();
+                return new
+                {
+                    ComplaintSubject = areaList.Last().Title,
+                    TraceScript = areaList.First().Href
+                };
+            })
+            .ToList();
+
+        var result = areas.Select(htmlData =>
+        {
+            var noiseLevelMatch =
+                System.Text.RegularExpressions.Regex.Match(htmlData.ComplaintSubject, @"(\d+(\.\d+)?) dBA");
+            var noiseLevel = double.Parse(noiseLevelMatch.Groups[1].Value,
+                System.Globalization.CultureInfo.InvariantCulture);
+
+            var dateMatch = System.Text.RegularExpressions.Regex.Match(htmlData.TraceScript, @"(\d{2}\.\d{2}\.\d{4})");
+            var timeMatch = System.Text.RegularExpressions.Regex.Match(htmlData.TraceScript, @"(\d{2}:\d{2}:\d{2})");
+            var timestampCet = DateTime.ParseExact(dateMatch.Groups[1].Value, "dd.MM.yyyy",
+                    System.Globalization.CultureInfo.InvariantCulture)
+                .Add(TimeSpan.ParseExact(timeMatch.Groups[1].Value, "hh\\:mm\\:ss",
+                    System.Globalization.CultureInfo.InvariantCulture));
+            var timestampUtc = TimeZoneInfo.ConvertTimeToUtc(timestampCet, _timeZoneCet);
+
+            return new NoiseMeasurement(timestampUtc, noiseLevel);
+        });
+
+        return Task.FromResult(result);
+    }
+}

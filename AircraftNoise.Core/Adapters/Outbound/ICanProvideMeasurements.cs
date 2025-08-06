eea95646e5899995faf5872f5c7c93e11f266316
Stefan Boos <kontakt@boos.systems>
2025-08-06 04:55:43 +0200

    feat: search noise level measurement candidates for single measurement with exact timestamp match

--- AircraftNoise.Core/Adapters/Outbound/ICanProvideMeasurements.cs
+++ AircraftNoise.Core/Adapters/Outbound/ICanProvideMeasurements.cs
@@ -4,5 +4,12 @@ namespace AircraftNoise.Core.Adapters.Outbound;
 
 public interface ICanProvideMeasurements
 {
-    Task<IEnumerable<NoiseMeasurement>> GetNoiseMeasurementsForPastTimePeriodAsync(DateTime endTimeUtc, TimeSpan duration);
+    /// <summary>
+    /// Read measurements for the past time period ending at the specified UTC time.
+    /// </summary>
+    /// <param name="endTimeUtc">Last timestamp to consider</param>
+    /// <param name="duration">Interval length. If zero, then return only an exact match of endTimeUtc</param>
+    /// <returns>List of measurements starting at or after endTimeUtc - duration and ending at or before endTimeUtc</returns>
+    Task<IEnumerable<NoiseMeasurement>> GetNoiseMeasurementsForPastTimePeriodAsync(DateTime endTimeUtc,
+        TimeSpan duration);
 }

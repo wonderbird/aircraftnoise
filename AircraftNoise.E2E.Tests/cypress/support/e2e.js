d8d80cbbdb16c105f081eaccde41b8214c09b41d
Stefan Boos <kontakt@boos.systems>
2025-09-15 11:26:57 +0200

    test(e2e): always run in Europe/Berlin timezone

--- AircraftNoise.E2E.Tests/cypress/support/e2e.js
+++ AircraftNoise.E2E.Tests/cypress/support/e2e.js
@@ -15,3 +15,15 @@
 
 // Import commands.js using ES2015 syntax:
 import "./commands";
+
+// Timestamp related tests are run for German timezone
+// This configuration affects both the developer's computer and the GitHub pipeline
+// https://github.com/cypress-io/cypress/issues/1043#issuecomment-2089006560
+Cypress.on("test:before:run", () => {
+  Cypress.automation("remote:debugger:protocol", {
+    command: "Emulation.setTimezoneOverride",
+    params: {
+      timezoneId: "Europe/Berlin",
+    },
+  });
+});
